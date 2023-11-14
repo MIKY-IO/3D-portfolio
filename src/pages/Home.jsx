861f3350b46b1a8c8a371f9745545f1dd7b858bf
Mikulas Richter <mikulasrichter@gmail.com>
2023-11-14 17:17:36 +0100

    sky set up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -48,6 +49,8 @@ const Home = () => {
             groundColor="#000000"
             intensity={1}
           />
+
+          <Sky />
 
           <Island
             position={islandPosition}
